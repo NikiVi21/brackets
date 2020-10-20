a3dd281ec31ce4d74c825cf60caaab13a1ad4a5c
Nikita Viktorov <nikita.viktorov21@gmail.com>
2020-10-21 01:56:56 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let br;
+    if (str.length % 2 !== 0) return false;
+
+    for (let i = 0; i < bracketsConfig.length; ) {
+        br = bracketsConfig[i].join("");
+        if (str.includes(br)) {
+            str = str.replace(br, "");
+            i = 0;
+        } else i++;
+    }
+    return str.length === 0 ? true : false;
+};
